c28ba592042ec30de76b62a5ab0fc73141d5f29e
KingNeero <124060792+KentDumagpi@users.noreply.github.com>
2023-03-05 21:54:20 +0800

    Add files via upload

---
+++ Palindrome_Dumagpi/Palindrome_Dumagpi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Dumagpi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input, reverse;
+
+            Console.Write("Enter a String: ");
+            input = Console.ReadLine();
+            input = input.Replace(" ", "");
+
+            char[] getVal = input.ToCharArray();
+            Array.Reverse(getVal);
+
+            reverse = new string(getVal);
+            bool test = input.Equals(reverse, StringComparison.CurrentCultureIgnoreCase);
+
+            if (test == true)
+            {
+                Console.WriteLine(input + " is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(input + " is not a Palindrome");
+            }
+            Console.Read();
+        }
+    }
+}
+        
